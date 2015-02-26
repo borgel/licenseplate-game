f61fc7731dd8aed05d5d79373c3af2e761bb0765
Kerry Scharfglass <iborgel@gmail.com>
2015-02-25 22:46:18 -0800

    finished regex version

--- licensegame-regex.py
+++ licensegame-regex.py
@@ -5,20 +5,26 @@ asdasd
 '''
 
 import sys
-
+import re
 
 USAGE = ''': [XYZ 3 letter sequence to lookup]'''
 
 def doLookup(letters):
-   print 'building table from the word list at ' + str(wordlist)
+   print 'regexing from the word list at ' + str(wordlist)
    wlist = open(wordlist, 'r')
+
+   reg = re.compile('.*?' + letters[0] + '.*?' + letters[1] + '.*?' + letters[2] + '.*?')
 
    # for every line in the file, add it to the reference table (without newline)
    for w in wlist:
       # remove whitespace and newlines
       w = w.strip()
 
-      #TODO FIXME run regex on w
+      found = reg.search(w)
+      if found != None:
+         #print found.span()
+         #print found.group()
+         print w
 
    wlist.close()
 
@@ -27,13 +33,11 @@ if len(sys.argv) < 2 or len(sys.argv) > 3:
    exit(0)
 
 # else we got a 3 letter combo to lookup
-letters = sys.argv[1]
+letters = sys.argv[2]
+wordlist = sys.argv[1]
 if len(letters) != 3:
    print 'You can only look up 3 letter combinations!'
    exit(1)
-
-
-#FIXME TODO build regex
 
 doLookup(letters)
 

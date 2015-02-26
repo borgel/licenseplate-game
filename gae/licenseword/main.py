a13f51bbc0bdb0766a1d096b2e519df9aef52b54
Kerry Scharfglass <iborgel@gmail.com>
2015-02-25 23:23:48 -0800

    GAE v1 functional\!

--- gae/licenseword/main.py
+++ gae/licenseword/main.py
@@ -1,25 +1,62 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
+# uhh, make this not shit
+
 import webapp2
+import re
+
+USAGE_MSG = '''
+asd
+'''
+
+GITHUB_DICT = 'https://raw.githubusercontent.com/borgel/licenseplate-game/master/wordlist-nodupes-ascii.txt'
+
+# do the actual game or whatever
+class QueryHandler(webapp2.RequestHandler):
+   def get(self, pattern):
+      if(len(pattern) != 3):
+         self.response.write("query wasn't 3 chars")
+         return
+
+      self.response.write("Searching for places with the ordered pattern: " + str(pattern))
+      self.response.write("<br>")
+
+      reg = re.compile('.*?' + pattern[0] + '.*?' + pattern[1] + '.*?' + pattern[2] + '.*?')
+
+      # open file
+      # for w in file
+      # w = w.strip()
+      # found = reg.search(w)
+      # if found != None:
+      #    #print found.span()
+      #    #print found.group()
+      #    print w
+      wlist = open('dictionaries/dict.txt')
+
+      hits = 0
+
+      # for every line in the file, add it to the reference table (without newline)
+      for w in wlist:
+         # remove whitespace and newlines
+         w = w.strip()
+
+         found = reg.search(w)
+         if found != None:
+            self.response.write(w + '<br>')
+            hits = hits + 1
+
+      wlist.close()
+
+      self.response.write('<br>')
+      self.response.write('Found ' + str(hits) + ' matching words')
+      self.response.write('<br>')
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+       # TODO make this print a form to fill out to execute the search
+        self.response.write(USAGE_MSG)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/search/(.*)', QueryHandler)
 ], debug=True)
